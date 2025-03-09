ce27d7813c39b79a2008618e481f2473b4171963
Shruthi Yenamagandla <yshruthi.14@gmail.com>
2025-03-09 00:18:17 -0600

    added timeline component, fixed aceternity issues, fixed framer motion

--- my-portfolio/app/page.js
+++ my-portfolio/app/page.js
@@ -1,37 +1,105 @@
 import { FaGithub, FaLinkedin, FaFileAlt} from "react-icons/fa";
+import React from "react";
+import { Timeline } from "@/components/ui/timeline";
+import Image from "next/image";
+
 
 export default function Home() {
+
+  const data = [
+    {
+      title: "SKILLS & TECHNOLOGIES",
+      content: (
+        <section id="Skills" className="bg-blue-200">
+          <div className="flex flex-col space-y-12 p-24"> 
+            <div id="sGreeting" className="font-chillax text-2xl"> heyy </div>
+            <div id="Name" className="font-chillax text-2xl"> shruthi <br/> yenamagandla </div>
+            <div id="sGreeting" className="font-chillax text-2xl"> heyy </div>
+            <div id="Name" className="font-chillax text-2xl"> shruthi <br/> yenamagandla </div>
+            <div id="sGreeting" className="font-chillax text-2xl"> heyy </div>
+            <div id="Name" className="font-chillax text-2xl"> shruthi <br/> yenamagandla </div>
+            <div id="sGreeting" className="font-chillax text-2xl"> heyy </div>
+            <div id="Name" className="font-chillax text-2xl"> shruthi <br/> yenamagandla </div>
+          </div>
+        </section>
+      ),
+    },
+
+    {
+      title: "PROJECTS",
+      content: (
+        <section id="Skills" className="bg-purple-200">
+          <div className="flex flex-col space-y-12 p-24"> 
+            <div id="sGreeting" className="font-chillax text-2xl"> heyy </div>
+            <div id="Name" className="font-chillax text-2xl"> shruthi <br/> yenamagandla </div>
+            <div id="sGreeting" className="font-chillax text-2xl"> heyy </div>
+            <div id="Name" className="font-chillax text-2xl"> shruthi <br/> yenamagandla </div>
+            <div id="sGreeting" className="font-chillax text-2xl"> heyy </div>
+            <div id="Name" className="font-chillax text-2xl"> shruthi <br/> yenamagandla </div>
+            <div id="sGreeting" className="font-chillax text-2xl"> heyy </div>
+            <div id="Name" className="font-chillax text-2xl"> shruthi <br/> yenamagandla </div>
+          </div>
+        </section>
+      ),
+    },
+
+    {
+      title: "ABOUT ME",
+      content: (
+        <section id="Skills" className="bg-green-200">
+          <div className="flex flex-col space-y-12 p-24"> 
+            <div id="sGreeting" className="font-chillax text-2xl"> heyy </div>
+            <div id="Name" className="font-chillax text-2xl"> shruthi <br/> yenamagandla </div>
+            <div id="sGreeting" className="font-chillax text-2xl"> heyy </div>
+            <div id="Name" className="font-chillax text-2xl"> shruthi <br/> yenamagandla </div>
+            <div id="sGreeting" className="font-chillax text-2xl"> heyy </div>
+            <div id="Name" className="font-chillax text-2xl"> shruthi <br/> yenamagandla </div>
+            <div id="sGreeting" className="font-chillax text-2xl"> heyy </div>
+            <div id="Name" className="font-chillax text-2xl"> shruthi <br/> yenamagandla </div>
+          </div>
+        </section>
+      ),
+    },
+  ];
+
+
   return (
-    <section id="Landing Page" className="w-screen h-screen bg-white">
-
-      <div className="flex flex-col space-y-28"> 
-        <div id="Navbar" className="w-screen top-0 py-5 px-18 border-black border-b-2"> 
-          <div id="SectionLinks" className="flex flex-row space-x-32 justify-between">
-            <div className="flex flex-row space-x-16"> 
-              <a href="#Skills" target="_blank" className="text-lg font-chillax"> SKILLS </a>
-              <a href="#Projects" target="_blank" className="text-lg font-chillax"> PROJECTS </a>
-              <a href="/resume.pdf" download className="text-lg font-chillax"> RESUME </a>
+    <>
+      <section id="Landing Page" className="w-screen h-screen bg-white">
+        <div className="flex flex-col space-y-28"> 
+          <div id="Navbar" className="w-screen top-0 py-5 px-18 border-black border-b-2"> 
+            <div id="SectionLinks" className="flex flex-row space-x-32 justify-between">
+              <div className="flex flex-row space-x-16"> 
+                <a href="#Skills" target="_blank" className="text-lg font-chillax"> SKILLS </a>
+                <a href="#Projects" target="_blank" className="text-lg font-chillax"> PROJECTS </a>
+                <a href="#AboutMe" target="_blank" className="text-lg font-chillax"> ABOUT ME </a>
+                <a href="/resume.pdf" download className="text-lg font-chillax"> RESUME </a>
+              </div>
+              <div id="SocialLinks" className="flex flex-row space-x-6"> 
+                <a href="https://www.linkedin.com/in/shruthi-yenamagandla-23883428b/" target="_blank"> 
+                  <FaLinkedin className="text-black text-3xl" /> 
+                </a>
+                <a href="https://github.com/Shruthi1935" target="_blank"> 
+                  <FaGithub className="text-black text-3xl" /> 
+                </a>            
+              </div>
             </div>
-            <div id="SocialLinks" className="flex flex-row space-x-6"> 
-              <a href="https://www.linkedin.com/in/shruthi-yenamagandla-23883428b/" target="_blank"> 
-                <FaLinkedin className="text-black text-3xl" /> 
-              </a>
-              <a href="https://github.com/Shruthi1935" target="_blank"> 
-                <FaGithub className="text-black text-3xl" /> 
-              </a>            
+          </div>
+
+          <div id="LandingPageContent">
+            <div className="flex flex-col space-y-72 p-18"> 
+              <div id="Greeting" className="font-camellia text-8xl self-end"> heyy </div>
+              <div id="Name" className="font-sugarmagic text-8xl"> shruthi <br/> yenamagandla </div>
             </div>
           </div>
         </div>
+      </section>
 
-        <div id="LandingPageContent">
-          <div className="flex flex-col space-y-72 p-18"> 
-            <div id="Greeting" className="font-camellia text-8xl self-end"> heyy </div>
-            <div id="Name" className="font-sugarmagic text-8xl"> shruthi <br/> yenamagandla </div>
-          </div>
+      <section id="Timeline"> 
+        <div className="w-full">
+          <Timeline data={data} />
         </div>
-      </div>
-
-
-    </section>
+      </section>
+    </>
   );
 }
