37d8c84f1eeb577659ad21ec11f1c8c75aa99a25
Shruthi Yenamagandla <yshruthi.14@gmail.com>
2025-03-09 20:07:17 -0500

    created project pages, routes, imgs, and completed 2/6

---
+++ my-portfolio/app/coogify/page.js
@@ -0,0 +1,28 @@
+import { SiPython, SiCplusplus, SiR, SiRstudioide, SiJavascript, SiTypescript, SiSqlite, SiHtml5, SiCss3, SiTailwindcss, SiReact, SiExpress, SiNodedotjs, SiNextdotjs, SiVite, SiMysql, SiFirebase, SiVercel, SiGit, SiGithub, SiNpm, SiFigma} from "react-icons/si";
+import { FaGithub, FaLinkedin, FaFileAlt, FaFile} from "react-icons/fa";
+
+export default function CoogifyPage() {
+    return (
+      <section id="CoogifyPage">  
+      <div className="flex flex-col space-y-8"> 
+        <div className="font-sugarmagic text-8xl text-black px-24 pt-20 pb-5"> Coogify Music </div>
+        <div className="flex flex-row space-x-12 mx-24">
+            <a href="http://3.18.22.13:5173/" target="_blank"> 
+                <img src="/coogify2.png" alt="Description" className="h-150 w-260 ml-auto rounded-md cursor-pointer hover:scale-101 duration-400"/>
+            </a>
+            <section className="space-y-8">
+                <div className="font-chillax text-2xl text-black"> Description </div>
+                <div className="flex flex-row space-x-5"> 
+                    <SiMysql className="text-black text-4xl cursor-pointer hover:-translate-y-2 duration-300" />
+                    <SiFirebase className="text-black text-4xl cursor-pointer hover:-translate-y-2 duration-300" />
+                    <SiVercel className="text-black text-4xl cursor-pointer hover:-translate-y-2 duration-300" />
+                    <SiGit className="text-black text-4xl cursor-pointer hover:-translate-y-2 duration-300" />
+                    <SiGithub className="text-black text-4xl cursor-pointer hover:-translate-y-2 duration-300" />            
+                </div>
+            </section>
+        </div>
+      </div>
+      </section>
+    );
+  }
+  
--- my-portfolio/app/page.js
+++ my-portfolio/app/page.js
@@ -3,7 +3,7 @@ import React from "react";
 import { Timeline } from "@/components/ui/timeline";
 import "./globals.css";
 import { SiPython, SiCplusplus, SiR, SiRstudioide, SiJavascript, SiTypescript, SiSqlite, SiHtml5, SiCss3, SiTailwindcss, SiReact, SiExpress, SiNodedotjs, SiNextdotjs, SiVite, SiMysql, SiFirebase, SiVercel, SiGit, SiGithub, SiNpm, SiFigma} from "react-icons/si";
-
+import Link from "next/link";
 
 
 export default function Home() {
@@ -15,79 +15,71 @@ export default function Home() {
         <section id="Skills" className="">
           <div className="flex flex-col space-y-16 py-24 pr-8 ml-auto w-max">
             <div className="flex flex-row space-x-20"> 
-              <SiPython className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300 hover:text-amber-400" />
-              <SiCplusplus className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300 hover:text-amber-500" />
-              <SiR className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300 hover:text-amber-500" />
-              <SiRstudioide className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300 hover:text-amber-500" />
-              <SiJavascript className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300 hover:text-amber-500" />            
+              <SiPython className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300" />
+              <SiCplusplus className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300" />
+              <SiR className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300" />
+              <SiRstudioide className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300" />
+              <SiJavascript className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300" />            
             </div>
             <div className="flex flex-row space-x-20"> 
-              <SiTypescript className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300 hover:text-amber-500" />
-              <SiSqlite className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300 hover:text-amber-500" />
-              <SiHtml5 className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300 hover:text-amber-500" />
-              <SiCss3 className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300 hover:text-amber-500" />
-              <SiTailwindcss className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300 hover:text-amber-500" />            
+              <SiTypescript className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300" />
+              <SiSqlite className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300" />
+              <SiHtml5 className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300" />
+              <SiCss3 className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300" />
+              <SiTailwindcss className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300" />            
             </div>
             <div className="flex flex-row space-x-20"> 
-              <SiReact className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300 hover:text-amber-500" />
-              <SiExpress className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300 hover:text-amber-500" />
-              <SiNodedotjs className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300 hover:text-amber-500" />
-              <SiNextdotjs className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300 hover:text-amber-500" />
-              <SiVite className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300 hover:text-amber-500" />            
+              <SiReact className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300" />
+              <SiExpress className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300" />
+              <SiNodedotjs className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300" />
+              <SiNextdotjs className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300" />
+              <SiVite className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300" />            
             </div>
             <div className="flex flex-row space-x-20"> 
-              <SiMysql className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300 hover:text-amber-500" />
-              <SiFirebase className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300 hover:text-amber-500" />
-              <SiVercel className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300 hover:text-amber-500" />
-              <SiGit className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300 hover:text-amber-500" />
-              <SiGithub className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300 hover:text-amber-500" />            
+              <SiMysql className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300" />
+              <SiFirebase className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300" />
+              <SiVercel className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300" />
+              <SiGit className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300" />
+              <SiGithub className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300" />            
             </div>
             <div className="flex flex-row space-x-20"> 
-              <SiNpm className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300 hover:text-amber-500" />
-              <SiFigma className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300 hover:text-amber-500" />     
+              <SiNpm className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300" />
+              <SiFigma className="text-black text-5xl cursor-pointer hover:-translate-y-3 duration-300" />     
             </div>
           </div>
         </section>
       ),
     },
 
+
     {
       title: "PROJECTS",
       content: (
         <section id="Projects" className="">
-          <div className="flex flex-col space-y-8 pr-8 ml-auto w-max"> 
-            <div className="flex flex-row space-x-8"> 
-              <a href="https://www.linkedin.com/in/shruthi-yenamagandla-23883428b/" target="_blank"> 
-                <img src="/globe.svg" alt="Description" className="bg-gray-200 h-64 w-64 rounded-md cursor-pointer hover:scale-110 duration-500"/>
-              </a>
-              <a href="https://www.linkedin.com/in/shruthi-yenamagandla-23883428b/" target="_blank"> 
-                <img src="/globe.svg" alt="Description" className="bg-gray-200 h-64 w-64 rounded-md cursor-pointer hover:scale-110 duration-500"/>
-              </a>
-              <a href="https://www.linkedin.com/in/shruthi-yenamagandla-23883428b/" target="_blank"> 
-                <img src="/globe.svg" alt="Description" className="bg-gray-200 h-64 w-64 rounded-md cursor-pointer hover:scale-110 duration-500"/>
-              </a>
+          <div className="flex flex-col space-y-6 pr-8 ml-auto w-max"> 
+            <div className="flex flex-row space-x-6"> 
+              <Link href="/coogify">
+                <img src="/coogify.png" alt="Description" className="bg-gray-200 h-56 w-96 rounded-md cursor-pointer hover:scale-105 duration-500"/>
+              </Link> 
+              <Link href="/portfolioOG">
+                <img src="/portfolio.png" alt="Description" className="bg-gray-200 h-56 w-96 rounded-md cursor-pointer hover:scale-105 duration-500"/>
+              </Link> 
             </div>
-            <div className="flex flex-row space-x-8"> 
-              <a href="https://www.linkedin.com/in/shruthi-yenamagandla-23883428b/" target="_blank"> 
-                <img src="/globe.svg" alt="Description" className="bg-gray-200 h-64 w-64 rounded-md cursor-pointer hover:scale-110 duration-500"/>
-              </a>
-              <a href="https://www.linkedin.com/in/shruthi-yenamagandla-23883428b/" target="_blank"> 
-                <img src="/globe.svg" alt="Description" className="bg-gray-200 h-64 w-64 rounded-md cursor-pointer hover:scale-110 duration-500"/>
-              </a>
-              <a href="https://www.linkedin.com/in/shruthi-yenamagandla-23883428b/" target="_blank"> 
-                <img src="/globe.svg" alt="Description" className="bg-gray-200 h-64 w-64 rounded-md cursor-pointer hover:scale-110 duration-500"/>
-              </a>
+            <div className="flex flex-row space-x-6"> 
+              <Link href="/about"> 
+                <img src="/volunteervault2.png" alt="Description" className="bg-gray-200 h-56 w-96 rounded-md cursor-pointer hover:scale-105 duration-500"/>
+              </Link> 
+              <Link href="/about">
+                <img src="/clarityai.png" alt="Description" className="bg-gray-200 h-56 w-96 rounded-md cursor-pointer hover:scale-105 duration-500"/>
+              </Link> 
             </div>
-            <div className="flex flex-row space-x-8"> 
-              <a href="https://www.linkedin.com/in/shruthi-yenamagandla-23883428b/" target="_blank"> 
-                <img src="/globe.svg" alt="Description" className="bg-gray-200 h-64 w-64 rounded-md cursor-pointer hover:scale-110 duration-500"/>
-              </a>
-              <a href="https://www.linkedin.com/in/shruthi-yenamagandla-23883428b/" target="_blank"> 
-                <img src="/globe.svg" alt="Description" className="bg-gray-200 h-64 w-64 rounded-md cursor-pointer hover:scale-110 duration-500"/>
-              </a>
-              <a href="https://www.linkedin.com/in/shruthi-yenamagandla-23883428b/" target="_blank"> 
-                <img src="/globe.svg" alt="Description" className="bg-gray-200 h-64 w-64 rounded-md cursor-pointer hover:scale-110 duration-500"/>
-              </a>
+            <div className="flex flex-row space-x-6"> 
+              <Link href="/about">
+                <img src="/ellie1.png" alt="Description" className="bg-gray-200 h-56 w-96 rounded-md cursor-pointer hover:scale-105 duration-500"/>
+              </Link> 
+              <Link href="/about"> 
+                <img src="/inventory3.png" alt="Description" className="bg-gray-200 h-56 w-96 rounded-md cursor-pointer hover:scale-105 duration-500"/>
+              </Link> 
             </div>
           </div>
         </section>
@@ -120,7 +112,7 @@ export default function Home() {
 
           <div id="LandingPageContent">
             <div className="flex flex-col space-y-72 p-18"> 
-              <div id="Greeting" className="font-camellia text-8xl self-end"> heyy </div>
+              <div id="Greeting" className="font-camellia text-8xl self-end p-8 hover:scale-120 duration-700"> heyy </div>
               <div id="Name" className="font-sugarmagic text-8xl"> shruthi <br/> yenamagandla </div>
             </div>
           </div>
@@ -138,14 +130,21 @@ export default function Home() {
       <section id="AboutMe" className="w-screen h-screen"> 
         <div className="font-sugarmagic text-8xl text-black px-30 pt-42 mb-8"> about me </div>
         <div className="font-chillax text-xl text-black w-screen px-30 leading-10 mb-10"> 
-          Nice to see you! I'm a junior at the University of Houston, <br/> pursuing Computer Science with a Mathematics minor. <br/>
+          Nice to see you! I'm a junior at the University of Houston, <br/> pursuing <span className="underline">Computer Science </span> with a <span className="underline"> Mathematics</span> minor. <br/>
           My projects and passion work showcase my curiosity, growth, <br/> and dedication — shaped by incredible professors and hands-on <br/> experience.
           Explore my journey, and feel free to check out my <br/> <div className="font-extrabold"> resume & socials below! </div>
         </div>
         <div className="flex flex-row space-x-5 px-28"> 
-          <FaLinkedin className="text-black text-6xl p-2 cursor-pointer hover:-translate-y-3 duration-300 hover:text-blue-700" />
-          <FaGithub className="text-black text-6xl p-2 cursor-pointer hover:-translate-y-3 duration-300" />
-          <FaFileAlt className="text-black text-6xl p-2 cursor-pointer hover:-translate-y-3 duration-300 hover:text-amber-400" />
+          <a href="https://www.linkedin.com/in/shruthi-yenamagandla-23883428b/" target="_blank"> 
+          <FaLinkedin className="text-black text-6xl p-2 cursor-pointer hover:-translate-y-3 duration-300 hover:text-blue-700" /> 
+          </a>
+          <a href="https://github.com/Shruthi1935" target="_blank"> 
+            <FaGithub className="text-black text-6xl p-2 cursor-pointer hover:-translate-y-3 duration-300" />
+          </a>
+          <a href="/resume.pdf" download > 
+            <FaFileAlt className="text-black text-6xl p-2 cursor-pointer hover:-translate-y-3 duration-300 hover:text-amber-400" />
+          </a>
+
         </div>
       </section>
     </>
